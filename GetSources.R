23d7bce3f81239e11e5f478591b06ccf1673e928
Vivien <vivien@macbook-pro-de-vivien.home>
2017-12-21 23:05:00 +0100

    Modif liens sources

--- GetSources.R
+++ GetSources.R
@@ -4,7 +4,12 @@
 require(tidyverse)
 
 download.file("https://drive.google.com/uc?export=download&id=1Y56k4ADcXehw9Ac07VuFMKKb0U1at78r",destfile = "Sources/Base_communes.csv")
-download.file("https://drive.google.com/uc?export=download&id=1KQDhDTkTvHpySsEhmDk7RcmibBfKJ_pR",destfile = "Sources/COMMUNE.dbf")
-download.file("https://drive.google.com/uc?export=download&id=1g6Ohx4bI1c4h2KhFNl1TXEz5BYOAEjcK",destfile = "Sources/COMMUNE.prj")
-download.file("https://drive.google.com/uc?export=download&id=1UTtSV5h6rXBSnPnvTG5i9izvHa-x_GKU",destfile = "Sources/COMMUNE.shp")
-download.file("https://drive.google.com/uc?export=download&id=1QePSRrQNmNCZ52n31M2ufMLD27zsI3aA",destfile = "Sources/COMMUNE.shx")
+download.file("https://drive.google.com/uc?export=download&id=1oUhBFweSLj-RTSVV5HR3o8jU2iPbGN7C",destfile = "Sources/COMMUNE.dbf")
+download.file("https://drive.google.com/uc?export=download&id=1avNoNdtNWqCWL1PNce0-EYRrcBtjDxl0",destfile = "Sources/COMMUNE.prj")
+download.file("https://drive.google.com/uc?export=download&id=1nr-UzBQ63-9SCCsLQ_GrtyCufEujU1Tl",destfile = "Sources/COMMUNE.shp")
+download.file("https://drive.google.com/uc?export=download&id=1i5vm8w3A1u5N_Urfu8D2Auuhx3r5jSWT",destfile = "Sources/COMMUNE.shx")
+
+
+
+# t<- st_read("Sources/COMMUNE.shp")
+# plot(st_geometry(t))
--- Init_Base.R
+++ Init_Base.R
@@ -5,17 +5,18 @@ require(data.table)
 require(FactoMineR)
 
 datacomm <- fread("Sources/Base_communes.csv",sep=";",dec=",",colClasses = c("REG"="chr"))
-mapCom <- st_read("Sources/COMMUNE.shp")
+mapCom <- st_read("Sources_orig/COMMUNE.shp")
 
 mapCom <- select(mapCom,-ID_GEOFLA, -starts_with("CODE"), -POPULATION,-starts_with("NOM")) %>%
-       rename(codgeo=INSEE_COM) %>% arrange(codgeo)
-contig <- st_intersects(mapCom,mapCom) # Matrice de contiguité
-contig <- lapply(contig,function(x) x[-1]) # On enlève le premier élément qui est la commune elle-même
+          rename(codgeo=INSEE_COM) %>% 
+          arrange(codgeo)
+contig <- st_intersects(mapCom,mapCom)      # Matrice de contiguité
+contig <- lapply(contig,function(x) x[-1])  # On enlève le premier élément qui est la commune elle-même
 
-iles <- which(sapply(contig,length) ==0) # On retire les iles monocommunales
+iles <- which(sapply(contig,length) ==0)    # On retire les iles monocommunales
 contig <- contig[-iles]
 mapCom  <- mapCom[-iles,]
-contig <- st_intersects(mapCom,mapCom) # On refait la matrice de contiguité
+contig <- st_intersects(mapCom,mapCom)      # On refait la matrice de contiguité sans les iles
 
 datacomm <- match(mapCom$codgeo,datacomm$CODGEO) %>% datacomm[.,]
 
@@ -28,15 +29,15 @@ plot(mapCom[contig[[1]][1],3],add=T,col="black")
 
 df_contig <- data.frame()
 selection <- select_if(datacomm,is.numeric) %>%
-              select(-SUPERF)  # Sélection des variables pour calculer la distance 
-acp <- PCA(selection,ncp=5,graph = F) # ACP pour calcul des distances sur coord factorielles
+              select(-SUPERF)              # Sélection des variables pour calculer la distance 
+acp <- PCA(selection,ncp=5,graph = F)      # ACP pour calcul des distances sur coord factorielles
 
 for (ii in 1:length(contig))
 {
   print(ii)
   aa <- expand.grid(ii,contig[[ii]])
   dd <- acp$ind$coord[c(ii,contig[[ii]]),] %>% dist() %>% as.matrix() # On calcule la distance entre les communes
-                                                                 # Sur la base des indicateurs de datacomm
+                                                                      # Sur la base des indicateurs de datacomm
   aa <- cbind(aa,dd[-1,1])
   df_contig <- rbind(df_contig,aa) 
 }
