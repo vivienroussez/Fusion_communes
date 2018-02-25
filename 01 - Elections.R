978e94238d43b0048547ac7f941e30c51988700f
Vivien <vivien@macbook-pro-de-vivien.home>
2018-02-25 11:07:23 +0100

    Ajout rapport elections

--- 01 - Elections.R
+++ 01 - Elections.R
@@ -17,10 +17,10 @@ var_voix_lib <- names(elect)[var_voix]
 
 # Changement de géogprahie : on passe du COG2017 au COG2015
 noms <- elect[,c(3,var_nom)] %>% 
-      changement_COG_typo(typos=var_nom_lib,codgeo_entree = "codgeo",annees = c(2016,2015))
+      changement_COG_typo(typos=var_nom_lib,codgeo_entree = "codgeo",annees = 2017:2015) 
 
 voix <- elect[,c(3,var_voix)] %>% 
-        changement_COG_varNum(var_num = var_voix_lib,codgeo_entree = "codgeo",annees = c(2016,2015))
+        changement_COG_varNum(var_num = var_voix_lib,codgeo_entree = "codgeo",annees = 2017:2015)
 
 
 # On reformate la table pour avoir un format "long". 
@@ -30,21 +30,24 @@ noms <- select(dat,codgeo,starts_with("nom")) %>%  gather(key = "nom",value = "c
 voix <- select(dat,codgeo,starts_with("voix")) %>%  gather(key = "voix",value = "nombre_voix",-codgeo)
 # Rapprochement des 2 
 dat <- cbind(noms,voix)[,-4]
-dat <- dat %>% filter( !substr(dat$codgeo,1,2) %in% c("ZZ","97"))
+dat <- dat %>% filter( !substr(dat$codgeo,1,2) %in% c("ZZ","97")) 
+
+biz <- grep("et",dat$candidat) # la fonction de COGugaison a donné des modalités nouvelles... On les enlève
+dat <- dat[-biz,]
 
 # On fait le tableau de contingence pour l'AFC avec factominer
 tab <- xtabs(data=dat,formula=nombre_voix~codgeo+candidat)
 # On élimine quelques communes pour lesquelles il n'y a pas de vote (hors blancs)
-# Sinon l'AFC ne passe pas 
 sans.vote <- which(apply(tab,MARGIN = 1,sum) ==0)
 tab <- tab[-sans.vote,]
+
 
 afc <- CA(tab,graph = F,ncp=4) 
 plot.CA(afc,invisible = "row")
 plot.CA(afc,invisible = "row",axes = 3:4)
 barplot(afc$eig[,1])
 
-coord <- data.frame(codgeo=rownames(afc$row$coord),afc$row$contrib)
+coord <- data.frame(codgeo=rownames(afc$row$coord),afc$row$coord)
 names(coord)[2:5] <- paste("Pol",1:4,sep="")
 
 # Vu l'éboulis des VP, on peut se contenter des deux premiers axes
