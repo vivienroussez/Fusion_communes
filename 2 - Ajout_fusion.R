8113d583bbed4f3e0cfb1c5283a78ab00646907f
Vivien <vivien@macbook-pro-de-vivien.home>
2018-03-01 07:11:29 +0100

    MAJ fusion + début ML

--- 2 - Ajout_fusion.R
+++ 2 - Ajout_fusion.R
@@ -22,27 +22,37 @@ PASSAGE_2015_2016_insee<-PASSAGE_2015_2016_insee
 fusion_2016_2017<-filter(PASSAGE_2016_2017_insee,typemodif=="f")
 nrow(fusion_2016_2017)
 ##suppression des communes qui n'ont pas bougées
-fusion_2016_2017<-subset(fusion_2016_2017,!cod2016==cod2017)
-nrow(fusion_2016_2017)
+#fusion_2016_2017<-subset(fusion_2016_2017,!cod2016==cod2017)
+#nrow(fusion_2016_2017)
 ##création d'un indicateur de fusion selon l'année
 temp<-split(fusion_2016_2017,fusion_2016_2017$cod2017)
 temp1<-lapply(temp,function(x) expand.grid(x[,1],x[,1]))
 fusion_2016_2017_bis<-bind_rows(temp1)
 
+####verif de la totalité des fusions sur 89405
+#toto<-fusion_2016_2017_bis[,Var1=="01172"]
+#toto<-select(fusion_2016_2017_bis,Var1=="01172")
+#toto<-subset(fusion_2016_2017_bis,Var1=="89405")
+#subset(fusion_2016_2017_bis,Var1=="89174")
+
+fusion_2016_2017_bis<-subset(fusion_2016_2017_bis,!Var1==Var2)
 fusion_2016_2017_bis$fusion_2016<-1
 fusion_2016_2017_bis
 
-couples_ebh<-merge(couples,fusion_2016_2017_bis,by.x=c("codgeo1","codgeo2"),by.y=c("Var1","Var2"),all.x=T)
-couples_ebh
-table(couples_ebh$fusion_2016)
+couples_ebh<-merge(couples,fusion_2016_2017_bis,
+                   by.x=c("codgeo1","codgeo2"),by.y=c("Var1","Var2"),all.x=T)
+couples_ebh$fusion_2016<-ifelse(is.na(couples_ebh$fusion_2016),yes=0,no=couples_ebh$fusion_2016)
+sum(couples_ebh$fusion_2016)
+
+
 
 ##De 2015 à 2016
 ##filtre sur le typemodif=f
 fusion_2015_2016<-filter(PASSAGE_2015_2016_insee,typemodif=="f")
 nrow(fusion_2015_2016)
 ##suppression des communes qui n'ont pas bougées
-fusion_2015_2016<-subset(fusion_2015_2016,!cod2015==cod2016)
-nrow(fusion_2015_2016)
+#fusion_2015_2016<-subset(fusion_2015_2016,!cod2015==cod2016)
+#nrow(fusion_2015_2016)
 ##création d'un indicateur de fusion selon l'année
 essai<-split(fusion_2015_2016,fusion_2015_2016$cod2016)
 essai1<-lapply(essai,function(x) expand.grid(x[,1],x[,1]))
@@ -51,19 +61,24 @@ fusion_2015_2016_bis<-bind_rows(essai1)
 fusion_2015_2016_bis$fusion_2015<-1
 fusion_2015_2016_bis
 
-couples_ebh<-merge(couples_ebh,fusion_2015_2016_bis,by.x=c("codgeo1","codgeo2"),by.y=c("Var1","Var2"),all.x=T)
-couples_ebh
-table(couples_ebh$fusion_2015)
-table(couples_ebh$fusion_2016)
+couples_ebh<-merge(couples_ebh,fusion_2015_2016_bis,
+                   by.x=c("codgeo1","codgeo2"),by.y=c("Var1","Var2"),all.x=T)
+couples_ebh$fusion_2015<-ifelse(is.na(couples_ebh$fusion_2015),yes=0,no=couples_ebh$fusion_2015)
+sum(couples_ebh$fusion_2015)
 
-couples_ebh$fusion_2015 <- ifelse(is.na(couples_ebh$fusion_2015),yes = 0, no = couples_ebh$fusion_2015)
-couples_ebh$fusion_2016 <- ifelse(is.na(couples_ebh$fusion_2016),yes = 0, no = couples_ebh$fusion_2016)
-table(couples_ebh$fusion_2015)
-table(couples_ebh$fusion_2016)
 
 #dédupliquer pour ne garder que les identifiants uniques
 couples_ebh$fusion <- couples_ebh$fusion_2015+couples_ebh$fusion_2016
-couples_ebh <- group_by(couples_ebh,ident) %>% summarise(fusion=sum(fusion))
+#sum(couples_ebh$fusion)
+#arrange(couples_ebh,ident)
+#sum(couples_ebh$fusion)
 
-base<-merge(base,couples_ebh,by.x="ident",by.y="ident",all.x=T)
+#subset(couples_ebh,fusion>=1)
+#couples_ebh <- group_by(couples_ebh,ident) %>% summarise(fusion=sum(fusion))
+doublons <- which(duplicated(couples_ebh$ident))
+couples_ebh_uniques <- couples_ebh[-doublons,]
+#sum(couples_ebh_uniques$fusion)
+
+base <- merge(base,couples_ebh_uniques,by.x="ident",by.y="ident",all.x=T)
+
 save(base,mapCom,couples,file="Base.RData")
---
+++ 4 - Machine learning.R
@@ -0,0 +1,145 @@
+require(tidyverse)
+require(randomForest)
+require(sf)
+require(bestglm) # Selection GLM
+require(glmnet) # Elasticnet
+require(GGally)
+require(FactoMineR)
+require(gbm)  # boosting
+require(e1071) # SVM
+require(rpart) # arbres
+require(caret) # Package pour sélection de paramètre par CV
+require(parallel)
+
+load("Base.RData")
+table(base$fusion)
+dat <- base
+summary(dat)
+
+## On vire les variables sur lesquelles il y a trop de données manquantes
+manquants <- sapply(dat,function(x) sum(is.na(x))) 
+lesquelles <- which(manquants>2000)
+dat <- dat[,-lesquelles]
+
+num  <- select(dat,starts_with("dist"),starts_with("nb")) 
+fact <- select(dat,-starts_with("dist"),-starts_with("nb"))
+
+# On remplace les NA par la moyenne et les NaN par le max 
+# (c'est quand on a un zéro et on considère la dist comme max dan ces cas)
+maxi <- sapply(num, max,na.rm=T)
+moy  <- sapply(num, mean,na.rm=T)
+
+for (ii in 1:ncol(num))
+{
+  num[is.na(num[,ii]),ii] <- moy[ii]
+  num[is.nan(num[,ii]),ii] <- maxi[ii]
+}
+
+cor(num)
+# acp <- PCA(num)
+
+dat <- cbind(fact,num) %>% 
+        mutate(y=as.factor(fusion/2)) %>%
+        select(-ident,-first,-second,-fusion)
+row.names(dat) <- fact$ident
+
+## On ne va prendre qu'un nombre restreint de ligne (tirées au hasard mais en gardant la proportion de fusion identique)
+## Sinon, les modèles mettent des plombes à tourner. Quand tout sera calé, on balancera sur toute la base
+
+set.seed(1234)
+
+echant <- group_by(dat,y) %>%
+  sample_frac(.1) %>% 
+  as.data.frame() ## Attention, bestglm ne passe pas si on laisse en tibble !!!
+
+row.names(echant) <- echant$ident
+
+nbloc <- 12 # parce ce que j'ai 4 coeurs et on va faire les simul sur 3
+bloc <- sample(1:nrow(echant)%%(nbloc+1))
+
+bloc.actif <- 1  
+don <- echant
+
+# Création d'une fonction qui va implémenter les modèles pour chaque bloc de CV
+# On l'excutera en parallèle une fois calée
+
+predit <- function(bloc.actif,don=dat) # Prend en para le DF en entrée et le bloc sur lequel faire la prév
+{
+  train <- don[bloc!=bloc.actif,]
+  test  <- don[bloc==bloc.actif,]
+  
+  XX <- model.matrix(y~.,data=train)
+  YY <- train$y
+  
+  XX.test <- model.matrix(y~.,data=test)
+  
+  ### GLM
+  mco <- glm(y~.,data=train,family = binomial)
+  mco.prev <- predict(mco,newdata=test,type="response") %>% as.data.frame() 
+  names(mco.prev)[1] <- "MCO"
+  
+  ### GLM avec choix des variables
+  mco.step <- step(mco)
+  mco.step.prev <- predict(mco.step,newdata=test,type="response") %>% as.data.frame() 
+  names(mco.step.prev)[1] <- "MCO.step"
+  
+  ### Lasso
+  lambda_lasso <- cv.glmnet(XX,YY,family="binomial",alpha=1)$lambda.1se ## Détermination auto du lambda
+  lasso <- glmnet(XX,YY,family="binomial",alpha=1,lambda = lambda_lasso)
+  lasso.prev <- predict(lasso,newx = XX.test,type="response") %>% as.data.frame() 
+  names(lasso.prev)[1] <- "LASSO"
+  
+  ### Ridge
+  lambda_ridge <- cv.glmnet(XX,YY,family="binomial",alpha=0)$lambda.1se ## Détermination auto du lambda
+  ridge <- glmnet(XX,YY,family="binomial",alpha=0,lambda = lambda_ridge)
+  ridge.prev <- predict(ridge,newx = XX.test,type="response") %>% as.data.frame() 
+  names(ridge.prev)[1] <- "RIDGE"
+  
+  ### Elasticnet
+  lambda_elastic <- cv.glmnet(XX,YY,family="binomial",alpha=.5)$lambda.1se ## Détermination auto du lambda
+  elastic <- glmnet(XX,YY,family="binomial",alpha=.5,lambda = lambda_elastic)
+  elastic.prev <- predict(elastic,newx = XX.test,type="response") %>% as.data.frame() 
+  names(elastic.prev)[1] <- "ELASTIC"
+  
+  ### Arbre
+  arbre <- rpart(y~.,data=train)
+  arbre.prev <- predict(arbre,newdata = test,type="prob")[,2] %>% as.data.frame()
+  names(arbre.prev) <- "ARBRE"
+  
+  ### Adaboost
+  ada <- gbm(y~.,data=train,distribution="adaboost",
+             interaction.depth=2,shrinkage=0.005,train.fraction=0.8,n.trees=100)
+  iter <- gbm.perf(ada)
+  ada <- gbm(y~.,data=train,distribution="adaboost",
+             interaction.depth=2,shrinkage=0.005,n.trees=iter)
+  ada.prev <- predict(ada,test,type="response",n.trees=iter) %>% as.data.frame()
+  names(ada.prev)[1] <- "ADABOOST"
+  
+  ### Forêt
+  rf <- randomForest(y~.,data=train,ntrees=500)
+  rf.prev <- predict(rf,newdata = test,type = "prob")[,2] %>% as.data.frame()
+  names(rf.prev)[1] <- "FORET"
+  
+  ### SVM
+  best.svm <- tune(svm,y~.,data=train,kernel="linear",
+                  ranges=list(cost=c(0.001,0.01,1,10,100,1000)),probability=T)
+  summary(best.svm)
+  svm.mod <- best.svm$best.model
+  #svm.mod <- svm(y~.,data=train,probability=T,cost=0.001)
+  svm.prev <- predict(svm.mod,newdata=test,probability=T)
+  
+  res <- data.frame(Y=test$y,SVM=svm.prev,GLM=mco.prev,BestGLM=mco.step.prev,lasso=lasso.prev,
+                   ridge=ridge.prev)
+  
+}
+
+predit(bloc.actif = 1,don=echant)
+
+
+logi  <- glm(y~.,family = binomial,data=echant)
+bestlog <- step(logi)
+bestlog2 <- bestglm(echant,family = binomial)
+
+
+
+
