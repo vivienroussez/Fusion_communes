56003f2a3eddf226979391a85295a4343162b217
vivien.roussez <vivien.roussez@developpement-durable.gouv.fr>
2018-03-20 13:12:57 +0100

    Some more lines to help interprete

--- 5 - Comparaisons.R
+++ 5 - Comparaisons.R
@@ -3,6 +3,7 @@ require(sf)
 require(pROC)
 require(data.table)
 require(caret)
+require(GGally)
 
 prev <- fread("PrevML.csv",sep=";",dec=',')[,-1] %>% as.data.frame() %>%
       mutate(Y=as.factor(Y))
@@ -13,3 +14,21 @@ prev.roc  <- lapply(prev[,-1],function(x) roc(prev$Y,x))
 prev.conf <- lapply(prev.factor, function(x) confusionMatrix(x,prev$Y))
 
 save(prev,prev.roc,prev.conf,file="DiagML.RData")
+
+## Exploitation des résultats pour comparaison des modèles
+
+plot(prev.roc$MCO.step)
+
+par(mfrow=c(3,3))
+lapply(prev.roc, auc)
+par(mfrow=c(1,1))
+
+plot(prev.roc$MCO)
+str(prev.roc[[1]])
+
+ggplot(prev,aes(x=DNN)) + geom_density()
+
+for (ii in seq(.1,.9,.1))
+{
+  (prev$MCO>ii) %>% sum() %>% print()
+}
